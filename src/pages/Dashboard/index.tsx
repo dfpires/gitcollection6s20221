1ce713a24984db06fcb2b0ac0bf41dc0c52cac6d
dfpires@gmail.com <dfpires@gmail.com>
2022-05-04 08:41:59 -0300

    04-05

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import logo from '../../assets/logo.svg'
+import {Title} from './styles'
+
 export const Dashboard:React.FC = () => {
     return (
         <> 
             <img src={logo} alt="Git Collection"/>
+            <Title> Catálogo de repositórios do Github </Title>
             
         </>
     )
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+// vamos criar o componente Title
+export const Title = styled.h1`
+    font-size: 48px;
+    color: #3a3a3a;
+    max-width: 500px;
+    line-height: 56px;
+    margin-top: 80px;
+`
